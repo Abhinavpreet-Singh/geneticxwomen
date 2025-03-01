a3e70beca6f0484fcf9116b35a3c2e23741b7335
prathammittall <prathamv0106@gmail.com>
2025-03-01 09:43:17 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './pages/components/Home/Navbar'
+import Hero from './pages/components/Home/Hero'
+import Services from './pages/components/Home/Services'
+import About from './pages/components/Home/About'
+import Contact from './pages/components/Home/Contact'
+import Footer from './pages/components/Home/Footer'
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Services />
+			<About />
+			<Contact />
+			<Footer />
+		</>
+	)
+}
+
+export default App
